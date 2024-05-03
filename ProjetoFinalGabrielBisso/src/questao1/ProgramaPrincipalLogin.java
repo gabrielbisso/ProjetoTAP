09e95d0bf5f0ed9411c5fe94408e71b7d54c4a61
Gabriel Bisso <56891367+gabrielbisso@users.noreply.github.com>
2024-05-03 18:12:55 -0300

    Adição dos arquivos por meio de upload

---
+++ ProjetoFinalGabrielBisso/src/questao1/ProgramaPrincipalLogin.java
@@ -0,0 +1,10 @@
+package questao1;
+
+public class ProgramaPrincipalLogin {
+	public static void main(String[] args) {
+		JanelaLogin j= new JanelaLogin();
+		j.setVisible(true);
+		Login l = new Login();
+		ControleLogin conLog = new ControleLogin(j, l);
+	}
+}
